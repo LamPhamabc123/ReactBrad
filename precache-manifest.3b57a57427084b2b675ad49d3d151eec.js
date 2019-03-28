f0478ecddfcc126af66e9aef462806218579de74
Lam Pham <lpham27@csc.com>
2019-03-28 10:28:43 +0700

    Updates

---
+++ precache-manifest.3b57a57427084b2b675ad49d3d151eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41f6874eb33c7c6ebda2",
+    "url": "/ReactBrad/static/js/runtime~main.d5b8d41a.js"
+  },
+  {
+    "revision": "55c94a7ea4a0922e755f",
+    "url": "/ReactBrad/static/js/main.a0efef04.chunk.js"
+  },
+  {
+    "revision": "665080cb638fb6e58b72",
+    "url": "/ReactBrad/static/js/2.88b5c646.chunk.js"
+  },
+  {
+    "revision": "55c94a7ea4a0922e755f",
+    "url": "/ReactBrad/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "665080cb638fb6e58b72",
+    "url": "/ReactBrad/static/css/2.3543e579.chunk.css"
+  },
+  {
+    "revision": "1acd536745176577191531684721fcab",
+    "url": "/ReactBrad/index.html"
+  }
+];
